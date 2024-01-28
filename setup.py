e5b1fff2fff522149fc27ff953cf2ec643132150
achaayb <achaaybdev@gmail.com>
2024-01-28 20:35:02 +0100

    Change version, Pydantic patch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pycoreapi',
     description="CoreAPI is a high-performance RSGI lightweight framework for building API's in python with no requirements",
-    version='0.1.1',
+    version='0.2.1',
     url='https://github.com/achaayb/CoreAPI',
     author='Ali Chaayb',
     author_email='achaayb.cheri.dev@gmail.com',
